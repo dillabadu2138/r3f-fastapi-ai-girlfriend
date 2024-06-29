a80643c72e5e825225647b4bab35991b219ca226
dillabadu2138 <dillabadu2138@gmail.com>
2024-06-29 16:01:11 +0900

    create a simple scene with mesh component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,15 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
+import Scene from "./components/Scene";
+
 function App() {
-  return <></>;
+  return (
+    <Canvas camera={{ position: [3, 3, 3], fov: 30 }}>
+      <Scene />
+      <OrbitControls />
+    </Canvas>
+  );
 }
 
 export default App;
